7e72be680a180d43c29344b2de58f0e82512e4d5
godwincybertechsolutions-cmyk <godwincybertechsolutions@gmail.com>
2025-11-17 15:04:21 +0300

    Add MobileCTA and GlobalSearch components to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { Testimonials } from './components/Testimonials';
 import { TrustSignals } from './components/TrustSignals';
+import { MobileCTA } from './components/MobileCTA';
+import { GlobalSearch } from './components/GlobalSearch';
 
 export default function App() {
   return (
     <div className="min-h-screen">
       <Header />
+      <MobileCTA />
+      <GlobalSearch />
       <main className="space-y-24">
         <Hero />
         <Properties />
