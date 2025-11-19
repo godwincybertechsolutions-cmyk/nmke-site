b530012ec58d8bba1797d459c86bc9812405ffbf
godwincybertechsolutions-cmyk <godwincybertechsolutions@gmail.com>
2025-11-19 16:44:23 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-  import "./styles/overrides.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./styles/overrides.css";
+import { BookingProvider } from "./components/booking/BookingProvider";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BookingProvider>
+    <App />
+  </BookingProvider>
+);
+
   
   
