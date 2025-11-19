68d0080e048bdc144294a30a32463a54530894c8
godwincybertechsolutions-cmyk <godwincybertechsolutions@gmail.com>
2025-11-19 16:28:59 +0300

    Create types.ts

---
+++ src/components/booking/types.ts
@@ -0,0 +1,15 @@
+export type PropertySummary = {
+  id?: number
+  title?: string
+  location?: string
+  price?: string
+  image?: string
+}
+
+export type SafariSummary = {
+  id?: number
+  title?: string
+  price?: string
+  duration?: string
+  image?: string
+}
